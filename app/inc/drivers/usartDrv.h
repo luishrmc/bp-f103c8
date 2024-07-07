ee19a00bbdfcf92039f4508913544fea8a5fb8b6
Luis <luishenrique.8804@gmail.com>
2024-07-07 13:31:49 -0300

    using inheritance in commCtrl class

--- app/inc/drivers/usartDrv.h
+++ app/inc/drivers/usartDrv.h
@@ -2,8 +2,8 @@
  * @file uartDriver.h
  * @author luis (luishenrique.8804@gmail.com)
  * @brief
- * @version 0.1
- * @date 2024-06-29
+ * @version 1.0
+ * @date 2024-07-07
  *
  * @copyright Copyright (c) 2024
  *
@@ -16,6 +16,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include <stdint.h>
 #include "queue.h"
+
 /* Private includes ----------------------------------------------------------*/
 
 /* Exported types ------------------------------------------------------------*/
@@ -28,9 +29,13 @@ typedef enum
 typedef struct
 {
     uInst inst;
-    uint8_t rxData;
-    Queue rx;
-    Queue tx;
+    uint8_t rxSize;
+    uint8_t txSize;
+    uint32_t txTimeout;
+    uint32_t rxTimeout;
+    uint8_t rxBuff[16];
+    uint8_t txBuff[16];
+    Queue rxQueue;
 } usartDrv_t;
 
 /* Exported constants --------------------------------------------------------*/
@@ -39,9 +44,13 @@ typedef struct
 
 /* Exported functions prototypes ---------------------------------------------*/
 void udInit(usartDrv_t *self, uInst instance);
-uint8_t udTx(usartDrv_t *self);
-uint8_t udRx(usartDrv_t *self, uint8_t *data);
+void udDefConf(usartDrv_t *self);
+uint8_t udRxIT(usartDrv_t *self);
+uint8_t udRxBlck(usartDrv_t *self);
+uint8_t udTxIT(usartDrv_t *self);
+uint8_t udTxBlck(usartDrv_t *self);
 
 /* Private defines -----------------------------------------------------------*/
 
 #endif /* __USARTDRV_H */
+
--- app/src/drivers/usartDrv.c
+++ app/src/drivers/usartDrv.c
@@ -2,14 +2,15 @@
  * @file uartDriver.c
  * @author luis (luishenrique.8804@gmail.com)
  * @brief
- * @version 0.1
- * @date 2024-07-06
+ * @version 1.0
+ * @date 2024-07-07
  *
  * @copyright Copyright (c) 2024
  *
  */
 
 /* Includes ------------------------------------------------------------------*/
+#include "mainCtrl.h"
 #include "usartDrv.h"
 #include <stdlib.h>
 #include "stm32f1xx_hal.h"
@@ -26,15 +27,20 @@
 #define USART1_PORT GPIOA
 
 /* Private macro -------------------------------------------------------------*/
-#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
+// declaração da macro OFFSETOF
+#define OFFSETOF(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
 
-#define container_of(ptr, type, member) ({            \
+// declaração da macro CONTAINER_OF
+#define CONTAINER_OF(ptr, type, member) ({            \
  const __typeof( ((type *)0)->member ) *__mptr = (ptr);    \
- (type *)( (char *)__mptr - offsetof(type,member) ); })
+ (type *)( (char *)__mptr - OFFSETOF(type,member) ); })
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
-uint8_t *uRxData1;
+UART_HandleTypeDef huart2;
+uInst *uInst1;
+
+/* Private function prototypes -----------------------------------------------*/
 
 /* Private user code ---------------------------------------------------------*/
 
@@ -42,9 +48,9 @@ void udInit(usartDrv_t *self, uInst instance)
 {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     memset(self, 0, sizeof(usartDrv_t));
-    initQueue(&self->rx);
-    initQueue(&self->tx);
+    initQueue(&self->rxQueue);
     self->inst = instance;
+    udDefConf(self);
 
     switch (instance)
     {
@@ -78,9 +84,8 @@ void udInit(usartDrv_t *self, uInst instance)
 
         HAL_NVIC_SetPriority(USART1_IRQn, 6, 0);
         HAL_NVIC_EnableIRQ(USART1_IRQn);
-
-        uRxData1 = &self->rxData;
-        HAL_UART_Receive_IT(&huart1, &self->rxData, 1);
+        uInst1 = &self->inst;
+        udRxIT(self);
         break;
     }
 
@@ -89,29 +94,82 @@ void udInit(usartDrv_t *self, uInst instance)
     }
 }
 
-uint8_t udTx(usartDrv_t *self)
+void udDefConf(usartDrv_t *self)
 {
-    uint8_t data;
+    self->txTimeout = HAL_MAX_DELAY;
+    self->rxTimeout = HAL_MAX_DELAY;
+    self->rxSize = 1;
+    self->txSize = 1;
+}
+
+uint8_t udRxIT(usartDrv_t *self)
+{
     switch (self->inst)
     {
     case DRV_USART1:
     {
-        if (dequeue(&self->tx, &data))
-            HAL_UART_Transmit(&huart1, (uint8_t *)&data, 1, 0xFFFF);
-        else
-            return 0;
+        return HAL_UART_Receive_IT(&huart1, self->rxBuff, self->rxSize);
         break;
     }
 
     default:
         break;
     }
-    return 1;
+    return HAL_ERROR;
 }
 
-uint8_t udRx(usartDrv_t *self, uint8_t *data)
+uint8_t udRxBlck(usartDrv_t *self)
 {
-    return dequeue(&self->rx, data);
+    switch (self->inst)
+    {
+    case DRV_USART1:
+    {
+        return HAL_UART_Receive(&huart1, self->rxBuff, self->rxSize, self->rxTimeout);
+        break;
+    }
+
+    default:
+        break;
+    }
+    return HAL_ERROR;
+}
+
+uint8_t udTxIT(usartDrv_t *self)
+{
+    switch (self->inst)
+    {
+    case DRV_USART1:
+    {
+        return HAL_UART_Transmit_IT(&huart1, self->txBuff, self->txSize);
+        break;
+    }
+
+    default:
+        break;
+    }
+    return HAL_ERROR;
+}
+
+uint8_t udTxBlck(usartDrv_t *self)
+{
+    switch (self->inst)
+    {
+    case DRV_USART1:
+    {
+        return HAL_UART_Transmit(&huart1, self->txBuff, self->txSize, self->txTimeout);
+        break;
+    }
+
+    default:
+        break;
+    }
+    return HAL_ERROR;
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+    /* Prevent unused argument(s) compilation warning */
+    UNUSED(huart);
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
@@ -121,8 +179,9 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
     usartDrv_t *uart;
     if (huart->Instance == USART1)
     {
-        uart = container_of(uRxData1, usartDrv_t, rxData);
+        uart = CONTAINER_OF(uInst1, usartDrv_t, inst);
+        for (uint8_t idx = 0; idx < uart->rxSize; idx++)
+            enqueue(&uart->rxQueue, uart->rxBuff[idx]);
     }
-    enqueue(&uart->rx, uart->rxData);
-    HAL_UART_Receive_IT(huart, &uart->rxData, 1);
+    udRxIT(uart);
 }
